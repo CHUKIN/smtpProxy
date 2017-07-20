90b852f2cca8d72a52279e0647ab7282cba51fdb
Atamanchuk_DN <Atamanchuk_DN@STWS1644>
2017-07-20 10:24:40 +0300

    Add project files.

---
+++ smtpProxy/Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace smtpProxy.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Send()
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("dmitriyatamanchuk@gmail.com", "dima852963?"),
+                EnableSsl = true
+            };
+            client.Send("dmitriyatamanchuk@gmail.com", "id61899437-02ac7a125@vkmessenger.com", "test", "testbody");
+            return Content("Sent");
+        }
+    }
+}
---
+++ smtpProxy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="smtpProxy.MvcApplication" Language="C#" %>
